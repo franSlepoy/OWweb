e854a7bd5c78d203fa12ff30dbae2d5134851ddc
axelriveroc <riverocoronelaxel@gmail.com>
2024-01-09 15:50:59 -0300

    cambios

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -3,21 +3,65 @@ import Proyectos from "../proyectos/Proyectos";
 import NavBar from "../../commond/navBar/NavBar";
 import StudioContainer from "../studio/StudioContainer";
 import Image_init from "./imagen_init/Image_init";
+import styles from "./Home.module.css"; // Importa el archivo de estilos CSS Module
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [isLogoHidden, setIsLogoHidden] = useState(false);
+  const [scrollDirection, setScrollDirection] = useState("down"); // 'down' o 'up'
+
+  // Función para manejar el evento de scroll
+  let lastScrollTop = 0; // Fuera de tu componente, para rastrear la última posición de scroll
+
+  const handleScroll = () => {
+    const currentScrollTop = window.scrollY;
+
+    // Ocultar o mostrar el logo basado en la posición de scroll
+    if (currentScrollTop > 100) {
+      // Supongamos que 100 es el umbral
+      setIsLogoHidden(true);
+    } else {
+      setIsLogoHidden(false);
+    }
+
+    // Determinar la dirección del scroll
+    if (currentScrollTop > lastScrollTop) {
+      // Scroll hacia abajo
+      setScrollDirection("down");
+    } else {
+      // Scroll hacia arriba
+      setScrollDirection("up");
+    }
+    lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop; // Para evitar valores negativos
+  };
+
+  // Usar useEffect para suscribirse y desuscribirse del evento de scroll
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
       {/* IMAGEN DE ARRIBA CON EL LOGO */}
       <Image_init />
       {/* logo */}
       <Box
+        /* className={`${styles.stickyLogo} ${
+          isLogoHidden ? styles.hideLogo : ""
+        }`} */
+        className={`${styles.stickyLogo} ${
+          isLogoHidden
+            ? scrollDirection === "down"
+              ? styles.fadeAndDrop
+              : styles.fadeAndRise
+            : ""
+        }`}
         sx={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
           zIndex: 1300,
-          /* opacity: isTitleVisible ? 1 : 0, */
           transition: "opacity 0.3s",
         }}
       >
